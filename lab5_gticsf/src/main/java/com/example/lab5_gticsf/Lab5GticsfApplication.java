01cefbd60ae8bbb4b7f3f3ba7d7baa00b6543a55
blvck29 <a20210850@pucp.edu.pe>
2024-05-08 19:15:08 -0500

    feat: create project

---
+++ lab5_gticsf/src/main/java/com/example/lab5_gticsf/Lab5GticsfApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab5_gticsf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab5GticsfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab5GticsfApplication.class, args);
+    }
+
+}
---
+++ lab5_gticsf/src/test/java/com/example/lab5_gticsf/Lab5GticsfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab5_gticsf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab5GticsfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
