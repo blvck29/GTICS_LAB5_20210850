c487f39ca84d9a1ab45bae1ed34f4618c5867b93
blvck29 <a20210850@pucp.edu.pe>
2024-05-08 20:21:15 -0500

    feat: crear y editar tecnico

--- lab5_gticsf/src/main/java/com/example/lab5_gticsf/Controller/AppController.java
+++ lab5_gticsf/src/main/java/com/example/lab5_gticsf/Controller/AppController.java
@@ -5,8 +5,12 @@ import com.example.lab5_gticsf.Repository.TechnicianRepo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 @Controller
 public class AppController {
@@ -16,7 +20,7 @@ public class AppController {
         this.technicianRepo = technicianRepo;
     }
 
-    @GetMapping(value = "")
+    @GetMapping(value = {"", "/"})
     public String inicio(){
         return "inicio";
     }
@@ -27,6 +31,33 @@ public class AppController {
         model.addAttribute("listaTecnicos", listaTecnicos);
 
         return "tecnicos";
+    }
+
+    @GetMapping(value = {"/new_tec"})
+    public String crearTecnico(@ModelAttribute("tecnico") Technician tecnico, Model model){
+        ArrayList<Technician> managerList = (ArrayList<Technician>) technicianRepo.findAll();
+        return "tec_new";
+    }
+
+    @GetMapping(value = {"/edit_tec"})
+    public String editarTecnico(@ModelAttribute("employee") Technician tecnico, Model model, @RequestParam("id") int id){
+        Optional<Technician> optEmployee = technicianRepo.findById(id);
+
+        if(optEmployee.isPresent()){
+            tecnico = optEmployee.get();
+            model.addAttribute("tecnico",tecnico);
+            return "tec_new";
+        }else{
+            return "redirect:/tecnicos";
+        }
+
+    }
+
+    @PostMapping("/save_tec")
+    public String guardarTecnico(@ModelAttribute("employee") Technician tecnico){
+
+        technicianRepo.save(tecnico);
+        return "redirect:/employee";
     }
 
     @GetMapping(value = "estadisticas")
